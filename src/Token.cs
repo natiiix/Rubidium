2328ea7abfc1049b7641b339e5e31f81708fc1f4
natiiix <natiwastaken@gmail.com>
2019-01-16 17:16:02 +0100

    Implement Token class

---
+++ src/Token.cs
@@ -0,0 +1,12 @@
+namespace Rubidium
+{
+    public class Token
+    {
+        public string Value { get; }
+
+        public Token(string value)
+        {
+            Value = value;
+        }
+    }
+}
