11d008c116c336131796f80c50f2b9ea9af7944e
natiiix <natiwastaken@gmail.com>
2019-01-16 17:12:13 +0100

    Implement Fraction normalization method and double-to-Fraction type cast

--- src/Fraction.cs
+++ src/Fraction.cs
@@ -5,24 +5,67 @@ namespace Rubidium
 {
     public class Fraction
     {
-        public BigInteger Numerator { get; }
-        public BigInteger Denominator { get; }
+        public BigInteger Numerator { get; private set; }
+        public BigInteger Denominator { get; private set; }
 
         public Fraction(BigInteger numerator, BigInteger denominator)
         {
-            if (denominator == 0)
+            if (denominator.IsZero)
             {
                 throw new DivideByZeroException();
             }
 
             Numerator = numerator;
             Denominator = denominator;
+
+            Normalize();
+        }
+
+        private void Normalize()
+        {
+            if (Denominator.Sign < 0)
+            {
+                Denominator = -Denominator;
+                Numerator = -Numerator;
+            }
+
+            BigInteger gcd = BigInteger.GreatestCommonDivisor(Numerator, Denominator);
+
+            if (!gcd.IsOne)
+            {
+                Numerator /= gcd;
+                Denominator /= gcd;
+            }
         }
 
         public Fraction(int numerator, int denominator = 1) : this((BigInteger)numerator, (BigInteger)denominator) { }
 
+        public static Fraction FromDouble(double value)
+        {
+            const int PRECISION = 15;
+            const int EXPONENT_VALUE_LENGTH = 4;
+            const int FULL_EXPONENT_LENGTH = EXPONENT_VALUE_LENGTH + 1;
+
+            string valueString = value.ToString($"e{PRECISION}");
+            int decimalSeparatorIndex = valueString.Length - PRECISION - FULL_EXPONENT_LENGTH - 1;
+
+            string mantissaString = valueString.Substring(0, decimalSeparatorIndex) + valueString.Substring(decimalSeparatorIndex + 1, PRECISION);
+            string exponentString = valueString.Substring(valueString.Length - EXPONENT_VALUE_LENGTH, EXPONENT_VALUE_LENGTH);
+
+            int exponent = int.Parse(exponentString) - PRECISION;
+
+            BigInteger numerator = BigInteger.Parse(mantissaString) * (exponent >= 0 ? PowerOf10(exponent) : 1);
+            BigInteger denominator = exponent < 0 ? PowerOf10(-exponent) : 1;
+
+            return new Fraction(numerator, denominator);
+        }
+
         public override string ToString() => $"({Numerator}/{Denominator})";
 
         public static explicit operator double(Fraction f) => (double)f.Numerator / (double)f.Denominator;
+
+        public static explicit operator Fraction(double d) => FromDouble(d);
+
+        private static BigInteger PowerOf10(int power) => BigInteger.Pow(10, power);
     }
 }
