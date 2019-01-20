b868ae436ab1522076b5384d0f0223e318716f1f
natiiix <natiwastaken@gmail.com>
2019-01-21 00:15:45 +0100

    Implement basic operations on Vector

--- src/Vector.cs
+++ src/Vector.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 namespace Rubidium
 {
     public class Vector
@@ -5,6 +8,7 @@ namespace Rubidium
         public Fraction[] Values { get; }
 
         public int Size => Values.Length;
+        public bool IsZero => Values.All(x => x.IsZero);
 
         public Fraction this[int index]
         {
@@ -16,5 +20,50 @@ namespace Rubidium
         {
             Values = values;
         }
+
+        public static Vector operator -(Vector v)
+        {
+            Fraction[] values = new Fraction[v.Size];
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                values[i] = -v[i];
+            }
+
+            return new Vector(values);
+        }
+
+        public static Vector operator +(Vector first, Vector second)
+        {
+            if (first.Size != second.Size)
+            {
+                throw new ArgumentException();
+            }
+
+            Fraction[] values = new Fraction[first.Size];
+
+            for (int i = 0; i < first.Size; i++)
+            {
+                values[i] = first[i] + second[i];
+            }
+
+            return new Vector(values);
+        }
+
+        public static Vector operator -(Vector first, Vector second) => first + -second;
+
+        public static Vector operator *(Vector v, Fraction f)
+        {
+            Fraction[] values = new Fraction[v.Size];
+
+            for (int i = 0; i < v.Size; i++)
+            {
+                values[i] = v[i] * f;
+            }
+
+            return new Vector(values);
+        }
+
+        public static Vector operator /(Vector v, Fraction f) => v * ~f;
     }
 }
