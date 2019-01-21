61a7dcf78bf10b1888692f8a06247150b2540418
natiiix <natiwastaken@gmail.com>
2019-01-21 01:16:40 +0100

    Implement function for soliving sets of linear equations
    Used algorithm is Gaussian elimination without any checks
    
    Checks may or may not be added later

--- src/Expression/FunctionCallExpression.cs
+++ src/Expression/FunctionCallExpression.cs
@@ -63,8 +63,97 @@ namespace Rubidium
 
                 return Parameters.Last().Evaluate(variables);
             }
+            else if (FunctionName == "linear" && TrySolveLinearEquationSet(Parameters, variables, out Fraction linearResult))
+            {
+                return linearResult;
+            }
 
             throw new NotImplementedException($"Function \"{FunctionName}\" with {Parameters.Count} parameters is not implemented");
+        }
+
+        private static bool TrySolveLinearEquationSet(List<OperationExpression> parameters, Dictionary<string, Fraction> variables, out Fraction result)
+        {
+            int numberOfVars = FindNumberOfVariables(parameters.Count);
+
+            if (numberOfVars < 0)
+            {
+                result = 0;
+                return false;
+            }
+
+            string[] varNames = new string[numberOfVars];
+
+            for (int i = 0; i < numberOfVars; i++)
+            {
+                if (parameters[i].Values.Count == 1 && parameters[i].Values[0] is VariableExpression variable)
+                {
+                    varNames[i] = variable.Name;
+                }
+                else
+                {
+                    result = 0;
+                    return false;
+                }
+            }
+
+            Vector[] matrix = new Vector[numberOfVars];
+
+            for (int i = 0; i < numberOfVars; i++)
+            {
+                Fraction[] values = new Fraction[numberOfVars + 1];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    int index = numberOfVars + (i * values.Length) + j;
+                    values[j] = parameters[index].Evaluate(variables);
+                }
+
+                matrix[i] = new Vector(values);
+            }
+
+            for (int i = 1; i < numberOfVars; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    matrix[i] -= (matrix[i][j] / matrix[j][j]) * matrix[j];
+                }
+            }
+
+            for (int i = numberOfVars - 2; i >= 0; i--)
+            {
+                for (int j = numberOfVars - 1; j > i; j--)
+                {
+                    matrix[i] -= (matrix[i][j] / matrix[j][j]) * matrix[j];
+                }
+            }
+
+            for (int i = 0; i < numberOfVars; i++)
+            {
+                matrix[i] /= matrix[i][i];
+            }
+
+            for (int i = 0; i < numberOfVars; i++)
+            {
+                variables[varNames[i]] = matrix[i][numberOfVars];
+            }
+
+            result = numberOfVars;
+            return true;
+        }
+
+        private static int FindNumberOfVariables(int numberOfParams)
+        {
+            for (int i = 1; i < 1000; i++)
+            {
+                int iPlus1 = i + 1;
+
+                if ((iPlus1 * iPlus1) - 1 == numberOfParams)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
