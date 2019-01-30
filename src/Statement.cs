e309ed8138a4c491bbef20268a8b5e1970426ae9
natiiix <natiwastaken@gmail.com>
2019-01-30 03:46:23 +0100

    Remove Variables from Statement

--- src/Statement.cs
+++ src/Statement.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Rubidium
 {
     public class Statement
@@ -8,14 +5,10 @@ namespace Rubidium
         public Expression Left { get; }
         public Expression Right { get; }
 
-        public IEnumerable<string> Variables { get; }
-
         public Statement(Expression left, Expression right)
         {
             Left = left;
             Right = right;
-
-            Variables = new Expression[] { Left, Right }.SelectMany(x => x.Variables).Distinct();
         }
 
         public override string ToString() => $"{Left} = {Right}";
