22579d86b6a140928ca995d40434e581796c2b60
natiiix <natiwastaken@gmail.com>
2019-01-30 05:23:14 +0100

    Implement proper variable substitution for OperationExpression
    If the expression contains no variable, it will be evaluated

--- src/Expression/OperationExpression.cs
+++ src/Expression/OperationExpression.cs
@@ -38,7 +38,75 @@ namespace Rubidium
         public override Expression SubstituteVariables(Dictionary<string, Fraction> variableValues)
         {
             IEnumerable<Expression> expressions = Expressions.Select(x => x.SubstituteVariables(variableValues));
-            return new OperationExpression(expressions.ToList(), new List<Operation>(Operations));
+
+            if (expressions.All(x => x is LiteralExpression))
+            {
+                LinkedList<Fraction> values = new LinkedList<Fraction>(expressions.Select(x => (x as LiteralExpression).Value));
+                LinkedList<Operation> operations = new LinkedList<Operation>(Operations);
+
+                foreach (Operation op in OperationPrecedence)
+                {
+                    LinkedListNode<Fraction> leftNode = values.First;
+                    LinkedListNode<Operation> opNode = operations.First;
+
+                    while (opNode != null)
+                    {
+                        if (opNode.Value == op)
+                        {
+                            LinkedListNode<Fraction> rightNode = leftNode.Next;
+
+                            switch (op)
+                            {
+                                case Operation.Addition:
+                                    leftNode.Value += rightNode.Value;
+                                    break;
+
+                                case Operation.Subtraction:
+                                    leftNode.Value -= rightNode.Value;
+                                    break;
+
+                                case Operation.Multiplication:
+                                    leftNode.Value *= rightNode.Value;
+                                    break;
+
+                                case Operation.Division:
+                                    leftNode.Value /= rightNode.Value;
+                                    break;
+
+                                case Operation.Power:
+                                    leftNode.Value ^= rightNode.Value;
+                                    break;
+
+                                default:
+                                    throw new Exception("Invalid operation");
+                            }
+
+                            LinkedListNode<Operation> nextOpNode = opNode.Next;
+
+                            values.Remove(rightNode);
+                            operations.Remove(opNode);
+
+                            opNode = nextOpNode;
+                        }
+                        else
+                        {
+                            leftNode = leftNode.Next;
+                            opNode = opNode.Next;
+                        }
+                    }
+                }
+
+                if (values.Count != 1 || operations.Count != 0)
+                {
+                    throw new Exception("Unexpected post-evaluation state");
+                }
+
+                return new LiteralExpression(values.First.Value);
+            }
+            else
+            {
+                return new OperationExpression(expressions.ToList(), new List<Operation>(Operations));
+            }
         }
 
         public static Expression Multiply(List<Expression> expressions)
