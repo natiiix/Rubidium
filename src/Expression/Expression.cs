432035f0837bc1e68cbfe4bf096fd43e8e714ede
natiiix <natiwastaken@gmail.com>
2019-02-05 13:31:54 +0100

    Implement operators +, -, * on Expression

--- src/Expression/Expression.cs
+++ src/Expression/Expression.cs
@@ -9,5 +9,17 @@ namespace Rubidium
         public bool ContainsVariables => Variables.Count() > 0;
 
         public abstract Expression SubstituteVariables(Dictionary<string, Fraction> variableValues);
+
+        public static Expression operator -(Expression expr) =>
+            NegatedExpression.Build(expr);
+
+        public static Expression operator +(Expression first, Expression second) =>
+            AdditionExpression.Build(first, second);
+
+        public static Expression operator -(Expression first, Expression second) =>
+            AdditionExpression.Build(first, -second);
+
+        public static Expression operator *(Expression first, Expression second) =>
+            MultiplicationExpression.Build(first, second);
     }
 }
