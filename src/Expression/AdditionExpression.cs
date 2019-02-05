03119cf5abbc80eb378e1dd437bc298bbae315a5
natiiix <natiwastaken@gmail.com>
2019-02-05 12:12:39 +0100

    Make AdditionExpression independent on OperationExpression

--- src/Expression/AdditionExpression.cs
+++ src/Expression/AdditionExpression.cs
@@ -1,45 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rubidium
 {
-    public class AdditionExpression : OperationExpression
+    public class AdditionExpression : Expression
     {
-        private AdditionExpression(List<Expression> expressions) : base(expressions, Operation.Addition) { }
+        public Fraction Constant { get; }
+        public List<Expression> VariableParts { get; }
 
-        public static Expression Build(List<Expression> expressions)
+        public override IEnumerable<string> Variables { get; }
+
+        private AdditionExpression(Fraction constant, List<Expression> variableParts)
+        {
+            Constant = constant;
+            VariableParts = variableParts;
+
+            Variables = variableParts.GetVariables();
+        }
+
+        public static Expression Build(Fraction baseConstant, List<Expression> expressions)
         {
             if (expressions.Count == 0)
             {
-                throw new Exception("Unable to make addition expression from zero expressions");
-            }
-            else if (expressions.Count == 1)
-            {
-                return expressions[0];
+                return new LiteralExpression(baseConstant);
             }
 
-            Fraction literalPart = Fraction.Zero;
+            Fraction constantPart = baseConstant;
             List<Expression> variableParts = new List<Expression>();
 
             foreach (Expression expr in expressions)
             {
                 if (expr is LiteralExpression literal)
                 {
-                    literalPart += literal.Value;
+                    constantPart += literal.Value;
                 }
                 else if (expr is AdditionExpression addition)
                 {
-                    foreach (Expression subExpr in addition.Expressions)
-                    {
-                        if (subExpr is LiteralExpression subLiteral)
-                        {
-                            literalPart += subLiteral.Value;
-                        }
-                        else
-                        {
-                            variableParts.Add(subExpr);
-                        }
-                    }
+                    constantPart += addition.Constant;
+                    variableParts.AddRange(addition.VariableParts);
                 }
                 else
                 {
@@ -49,14 +48,22 @@ namespace Rubidium
 
             if (variableParts.Count == 0)
             {
-                return new LiteralExpression(literalPart);
+                return new LiteralExpression(constantPart);
             }
-            else if (literalPart != Fraction.Zero)
+            else if (constantPart == Fraction.Zero && variableParts.Count == 1)
             {
-                variableParts.Add(new LiteralExpression(literalPart));
+                return variableParts[0];
             }
 
-            return new AdditionExpression(variableParts);
+            return new AdditionExpression(constantPart, variableParts);
         }
+
+        public static Expression Build(List<Expression> expressions) => Build(Fraction.Zero, expressions);
+
+        public override Expression SubstituteVariables(Dictionary<string, Fraction> variableValues) =>
+            Build(Constant, VariableParts.Select(x => x.SubstituteVariables(variableValues)).ToList());
+
+        public override string ToString() =>
+            $"( {Constant} + {string.Join(" + ", VariableParts.Select(x => x.ToString()))} )";
     }
 }
