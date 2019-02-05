9ea86d5e8525fe6614b92e81113c1a712451cf12
natiiix <natiwastaken@gmail.com>
2019-02-05 12:05:09 +0100

    Make MultiplicationExpression independent on OperationExpression

--- src/Expression/MultiplicationExpression.cs
+++ src/Expression/MultiplicationExpression.cs
@@ -1,24 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rubidium
 {
-    public class MultiplicationExpression : OperationExpression
+    public class MultiplicationExpression : Expression
     {
-        private MultiplicationExpression(List<Expression> expressions) : base(expressions, Operation.Multiplication) { }
+        public Fraction Coefficient { get; }
+        public List<Expression> VariableParts { get; }
 
-        public static Expression Build(List<Expression> expressions)
+        public override IEnumerable<string> Variables { get; }
+
+        private MultiplicationExpression(Fraction coefficient, List<Expression> variableParts)
+        {
+            Coefficient = coefficient;
+            VariableParts = variableParts;
+
+            Variables = variableParts.GetVariables();
+        }
+
+        public static Expression Build(Fraction baseCoefficient, List<Expression> expressions)
         {
             if (expressions.Count == 0)
             {
                 throw new Exception("Unable to make multiplication expression from zero expressions");
             }
-            else if (expressions.Count == 1)
-            {
-                return expressions[0];
-            }
 
-            Fraction coefficient = Fraction.One;
+            Fraction coefficient = baseCoefficient;
             List<Expression> variableParts = new List<Expression>();
 
             foreach (Expression expr in expressions)
@@ -29,17 +37,8 @@ namespace Rubidium
                 }
                 else if (expr is MultiplicationExpression multiplication)
                 {
-                    foreach (Expression subExpr in multiplication.Expressions)
-                    {
-                        if (subExpr is LiteralExpression subLiteral)
-                        {
-                            coefficient *= subLiteral.Value;
-                        }
-                        else
-                        {
-                            variableParts.Add(subExpr);
-                        }
-                    }
+                    coefficient *= multiplication.Coefficient;
+                    variableParts.AddRange(multiplication.VariableParts);
                 }
                 else
                 {
@@ -51,12 +50,20 @@ namespace Rubidium
             {
                 return new LiteralExpression(coefficient);
             }
-            else if (coefficient != Fraction.One)
+            else if (coefficient == Fraction.One && variableParts.Count == 1)
             {
-                variableParts.Add(new LiteralExpression(coefficient));
+                return variableParts[0];
             }
 
-            return new MultiplicationExpression(variableParts);
+            return new MultiplicationExpression(coefficient, variableParts);
         }
+
+        public static Expression Build(List<Expression> expressions) => Build(Fraction.One, expressions);
+
+        public override Expression SubstituteVariables(Dictionary<string, Fraction> variableValues) =>
+            Build(Coefficient, VariableParts.Select(x => x.SubstituteVariables(variableValues)).ToList());
+
+        public override string ToString() =>
+            $"( {Coefficient} * {string.Join(" * ", VariableParts.Select(x => x.ToString()))} )";
     }
 }
