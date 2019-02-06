71e422825e130d29fd4ef20d8c48111f511ea923
natiiix <natiwastaken@gmail.com>
2019-02-06 12:51:51 +0100

    Switch query to system of linear equations
    Solving systems of linear equations is the next thing to be implemented

--- src/Program.cs
+++ src/Program.cs
@@ -20,14 +20,14 @@ namespace Rubidium
                 // "2 = a; -8 = b; -24 = c"
 
                 // (x, y, z) = ( 3/10, 2/5, 0 )
-                // "2x + y + 3z = 1",
-                // "2x + 6y + 8z = 3",
-                // "6x + 8y + 18z = 5"
+                "2x + y + 3z = 1",
+                "2x + 6y + 8z = 3",
+                "6x + 8y + 18z = 5"
 
                 // "x = 5 + (7 - (5 + 12)) + 4"
                 // "x = 5 * (7 * (5 * 12)) * 4"
 
-                "10x - 5x + 10 = -15x + 20"
+                // "10x - 5x + 10 = -15x + 20"
             };
 
             List<Token> tokens = Lexer.Tokenize(string.Join(';', query));
