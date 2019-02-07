f07c70d9c439e69bf2f80ee3baa3fcfb6fb116ba
natiiix <natiwastaken@gmail.com>
2019-02-07 17:14:33 +0100

    Add Context test: system of linear equations

--- Rubidium.Tests/src/ContextTests.cs
+++ Rubidium.Tests/src/ContextTests.cs
@@ -18,5 +18,24 @@ namespace Rubidium.Tests
             Assert.True(c.VariableValues.ContainsKey("x"));
             Assert.Equal(new Fraction(1, 2), c.VariableValues["x"]);
         }
+
+        [Fact]
+        public static void TestSystemOfLinearEquations()
+        {
+            Context c = Program.Evaluate("2x + y + 3z = 1; 2x + 6y + 8z = 3; 6x + 8y + 18z = 5");
+
+            Assert.Empty(c.Statements);
+            Assert.Empty(c.VariableExpressions);
+
+            Assert.Equal(3, c.VariableValues.Count);
+
+            Assert.True(c.VariableValues.ContainsKey("x"));
+            Assert.True(c.VariableValues.ContainsKey("y"));
+            Assert.True(c.VariableValues.ContainsKey("z"));
+
+            Assert.Equal(new Fraction(3, 10), c.VariableValues["x"]);
+            Assert.Equal(new Fraction(2, 5), c.VariableValues["y"]);
+            Assert.Equal(Fraction.Zero, c.VariableValues["z"]);
+        }
     }
 }
