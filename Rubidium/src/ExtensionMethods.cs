d8f85be8c99c2ea2dbd76ccc0f6aeef2b1162406
natiiix <natiwastaken@gmail.com>
2019-02-07 18:34:43 +0100

    Add extension method for stripping outmost pair of parentheses

--- Rubidium/src/ExtensionMethods.cs
+++ Rubidium/src/ExtensionMethods.cs
@@ -7,5 +7,8 @@ namespace Rubidium
     {
         internal static IEnumerable<string> GetVariables(this IEnumerable<ICanContainVariables> objects) =>
             objects.SelectMany(x => x.Variables).Distinct();
+
+        internal static string StripParentheses(this string str) =>
+            str.StartsWith('(') && str.EndsWith(')') ? StripParentheses(str.Substring(1, str.Length - 2)) : str;
     }
 }
