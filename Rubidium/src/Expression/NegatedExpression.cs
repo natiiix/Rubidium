f820795c5098db15181e3b20c38b3876685d3294
natiiix <natiwastaken@gmail.com>
2019-02-07 17:51:06 +0100

    Remove parentheses from NegatedExpression string

--- Rubidium/src/Expression/NegatedExpression.cs
+++ Rubidium/src/Expression/NegatedExpression.cs
@@ -41,6 +41,6 @@ namespace Rubidium
         public override Expression SubstituteVariables(Dictionary<string, Fraction> variableValues, Dictionary<string, Expression> variableExpressions) =>
             Build(Expression.SubstituteVariables(variableValues, variableExpressions));
 
-        public override string ToString() => $"-({Expression})";
+        public override string ToString() => $"-{Expression}";
     }
 }
