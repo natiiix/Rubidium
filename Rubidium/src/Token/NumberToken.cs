b80469cb7c07b3a3b1f87358a9770d94d619eccf
natiiix <natiwastaken@gmail.com>
2019-02-09 19:19:08 +0100

    Add docstrings to NumberToken

--- Rubidium/src/Token/NumberToken.cs
+++ Rubidium/src/Token/NumberToken.cs
@@ -1,11 +1,21 @@
-using System.Numerics;
-
 namespace Rubidium
 {
+    /// <summary>
+    /// Class representing a token with a literal numeric value.
+    /// </summary>
     public class NumberToken : Token
     {
+        /// <summary>
+        /// Numeric value of the token.
+        /// </summary>
         public Fraction NumericValue { get; }
 
+        /// <summary>
+        /// NumberToken constructor, which calls the base Token constructor
+        /// and then parses the numeric value.
+        /// </summary>
+        /// <param name="str">Token string, which will be parsed to get the numeric value.</param>
+        /// <param name="index">Index of token in query string.</param>
         public NumberToken(string str, int index) : base(str, index)
         {
             NumericValue = Fraction.Parse(StringValue);
