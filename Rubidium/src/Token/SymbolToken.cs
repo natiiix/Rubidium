3c5b9d636538790f8e496bef72af6807b340907d
natiiix <natiwastaken@gmail.com>
2019-02-09 19:13:08 +0100

    Add docstrings to SymbolToken

--- Rubidium/src/Token/SymbolToken.cs
+++ Rubidium/src/Token/SymbolToken.cs
@@ -1,7 +1,15 @@
 namespace Rubidium
 {
+    /// <summary>
+    /// Class representing a single symbol (e.g., variable name).
+    /// </summary>
     public class SymbolToken : Token
     {
+        /// <summary>
+        /// SymbolToken constructor, which just calls the base Token constructor.
+        /// </summary>
+        /// <param name="str">Token string. This is the value of the symbol.</param>
+        /// <param name="index">Index of token in query string.</param>
         public SymbolToken(string str, int index) : base(str, index) { }
     }
 }
