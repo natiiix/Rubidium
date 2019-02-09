2eca4ddeda9e0d97a63c3b0ef4639ec3d587fd09
natiiix <natiwastaken@gmail.com>
2019-02-09 19:33:39 +0100

    Add docstrings to ICanContainVariables

--- Rubidium/src/Expression/ICanContainVariables.cs
+++ Rubidium/src/Expression/ICanContainVariables.cs
@@ -2,9 +2,18 @@ using System.Collections.Generic;
 
 namespace Rubidium
 {
+    /// <summary>
+    /// Interface for types that can contain variables (e.g., Expression, Statement).
+    /// </summary>
     interface ICanContainVariables
     {
+        /// <summary>
+        /// Enumerable of variables contained (references) in the object.
+        /// </summary>
         IEnumerable<string> Variables { get; }
+        /// <summary>
+        /// Boolean value indicating if the object contains any variable.
+        /// </summary>
         bool ContainsVariables { get; }
     }
 }
