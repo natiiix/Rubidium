59950a20dff334c50a845e4b79b0c6fe4918ef4b
natiiix <natiwastaken@gmail.com>
2019-02-09 14:38:13 +0100

    Modify LexerTests.TestFloat to use proper type check instead of typeof()

--- Rubidium.Tests/src/LexerTests.cs
+++ Rubidium.Tests/src/LexerTests.cs
@@ -14,19 +14,19 @@ namespace Rubidium.Tests
 
             Assert.Equal(5, tokens.Count);
 
-            Assert.IsType(typeof(SymbolToken), tokens[0]);
+            Assert.IsType<SymbolToken>(tokens[0]);
             Assert.Equal("x", (tokens[0] as SymbolToken).StringValue);
 
-            Assert.IsType(typeof(SpecialToken), tokens[1]);
+            Assert.IsType<SpecialToken>(tokens[1]);
             Assert.True((tokens[1] as SpecialToken).Equality);
 
-            Assert.IsType(typeof(NumberToken), tokens[2]);
+            Assert.IsType<NumberToken>(tokens[2]);
             Assert.Equal((Fraction)3.14159, (tokens[2] as NumberToken).NumericValue);
 
-            Assert.IsType(typeof(SpecialToken), tokens[3]);
+            Assert.IsType<SpecialToken>(tokens[3]);
             Assert.True((tokens[3] as SpecialToken).Subtraction);
 
-            Assert.IsType(typeof(NumberToken), tokens[4]);
+            Assert.IsType<NumberToken>(tokens[4]);
             Assert.Equal((Fraction)123456.7890, (tokens[4] as NumberToken).NumericValue);
         }
     }
