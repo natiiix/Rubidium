cb31412dabc95ad22aa399a13cb88f5416bc43dd
natiiix <natiwastaken@gmail.com>
2019-02-10 15:15:27 +0100

    Add docstrings to everything in Context, except for FindNewStatements()

--- Rubidium/src/Context.cs
+++ Rubidium/src/Context.cs
@@ -40,6 +40,15 @@ namespace Rubidium
         /// </summary>
         private IEnumerable<string> FreeVariables => Variables.Where(x => !VariableExpressions.ContainsKey(x) && !VariableValues.ContainsKey(x));
 
+        /// <summary>
+        /// Context class constructor.
+        /// Takes initial statements entered by user, extracts variables from them and
+        /// prepares the Context for statement evaluation.
+        /// </summary>
+        /// <param name="initialStatements">Initial statements entered by user.
+        /// These statements will be evaluated by the Context.</param>
+        /// <param name="verbose">Boolean value indicating if the Context should be verbose.
+        /// Being verbose means printing a summary at the end of each iteration of statement evaluation.</param>
         public Context(List<Statement> initialStatements, bool verbose = true)
         {
             Verbose = verbose;
@@ -75,7 +84,7 @@ namespace Rubidium
                     newStatements.Add(s.SubstituteVariables(VariableValues, VariableExpressions));
                 }
                 else if (s.Left is VariableExpression leftVariable &&
-                    IsVariableExpression(s.Right, leftVariable.Name))
+                    IsVariableExpressionRightSide(s.Right, leftVariable.Name))
                 {
                     VariableExpressions[leftVariable.Name] = s.Right;
                 }
@@ -85,12 +94,12 @@ namespace Rubidium
                 }
                 else if (s.Left is MultiplicationExpression leftMultiplication &&
                     leftMultiplication.IsVariableWithCoefficient &&
-                    IsVariableExpression(s.Right, leftMultiplication.VariableName))
+                    IsVariableExpressionRightSide(s.Right, leftMultiplication.VariableName))
                 {
                     VariableExpressions[leftMultiplication.VariableName] = s.Right / leftMultiplication.Coefficient;
                 }
                 else if (s.Left is AdditionExpression leftAddition &&
-                    (s.Right is AdditionExpression || !leftAddition.Constant.IsZero || leftAddition.VariableParts.Any(x => IsUsableVariable(x, expressedVariables))))
+                    (s.Right is AdditionExpression || !leftAddition.Constant.IsZero || leftAddition.VariableParts.Any(x => IsVariableExpressionLeftSide(x, expressedVariables))))
                 {
                     if (s.Right is AdditionExpression rightAddition)
                     {
@@ -114,7 +123,7 @@ namespace Rubidium
                     }
                     else
                     {
-                        Expression usableVar = leftAddition.VariableParts.Find(x => IsUsableVariable(x, expressedVariables));
+                        Expression usableVar = leftAddition.VariableParts.Find(x => IsVariableExpressionLeftSide(x, expressedVariables));
 
                         newStatements.Add(new Statement(
                             usableVar,
@@ -200,14 +209,35 @@ namespace Rubidium
             return (newStatements.Count > 0 || (newVariablesValues > 0 && (keepStatements.Count > 0 || newVarExpressions.Count > 0)));
         }
 
-        private bool IsUsableVariable(Expression expr, List<string> expressedVariables) =>
-            (expr is VariableExpression varExpr && FreeVariables.Contains(varExpr.Name) && !expressedVariables.Contains(varExpr.Name)) ||
-            (expr is MultiplicationExpression multiExpr && multiExpr.IsVariableWithCoefficient &&
+        /// <summary>
+        /// Determines if the given expression is a single-variable expression, from which the variable can be expressed.
+        /// This must either be a direct variable expression or a multiplication expression containing
+        /// a single variable expression a constant coefficient.
+        /// The variable must also be free (not bound to expression or value) and it must not have been expressed yet in this iteration.
+        /// </summary>
+        /// <param name="leftExpr">Left-side expression containing a single variable.</param>
+        /// <param name="expressedVariables">Names of variables already expressed in this iteration.</param>
+        /// <returns>Returns boolean value indicating if the given expression can be used for expressing a variable.</returns>
+        private bool IsVariableExpressionLeftSide(Expression leftExpr, List<string> expressedVariables) =>
+            (leftExpr is VariableExpression varExpr && FreeVariables.Contains(varExpr.Name) && !expressedVariables.Contains(varExpr.Name)) ||
+            (leftExpr is MultiplicationExpression multiExpr && multiExpr.IsVariableWithCoefficient &&
                 FreeVariables.Contains(multiExpr.VariableName) && !expressedVariables.Contains(multiExpr.VariableName));
 
-        private bool IsVariableExpression(Expression rightExpr, string varName) =>
+        /// <summary>
+        /// Determines if given expression is a valid variable expression.
+        /// This means that the variable to be expression is free (not bound to expression or constant value)
+        /// and at the same time the right-side expression does not contain a reference to this variable.
+        /// </summary>
+        /// <param name="rightExpr">Right-side expression.</param>
+        /// <param name="varName">Name of left-side variable to be expressed.</param>
+        /// <returns>Return boolean value indicating if specified variable can be expressed using given expression.</returns>
+        private bool IsVariableExpressionRightSide(Expression rightExpr, string varName) =>
             FreeVariables.Contains(varName) && !rightExpr.Variables.Contains(varName);
 
+        /// <summary>
+        /// Prints given string using Console.WriteLine() if Verbose flag is set.
+        /// </summary>
+        /// <param name="str">String to print.</param>
         private void PrintIfVerbose(string str)
         {
             if (Verbose)
@@ -216,19 +246,31 @@ namespace Rubidium
             }
         }
 
+        /// <summary>
+        /// Overload of verbose printing method to avoid having to call object.ToString().
+        /// </summary>
+        /// <param name="obj">Object to cast to string and print.</param>
         private void PrintIfVerbose(object obj) => PrintIfVerbose(obj.ToString());
 
+        /// <summary>
+        /// Override of object.ToString() method.
+        /// Generates a string representation of the current state of Context.
+        /// It contains all available statements, variable expressions and variable values in a formatted form.
+        /// </summary>
+        /// <returns>Returns a string representation of the current state of Context.</returns>
         public override string ToString()
         {
             const string LEFT_PADDING = "  ";
             string str = string.Empty;
 
+            // Add statements.
             if (Statements.Count > 0)
             {
                 str += "Statements:" + Environment.NewLine +
                     string.Join(Environment.NewLine, Statements.Select(x => $"{LEFT_PADDING}{x}"));
             }
 
+            // Add variable expressions.
             if (VariableExpressions.Count > 0)
             {
                 if (!string.IsNullOrEmpty(str))
@@ -240,6 +282,7 @@ namespace Rubidium
                     string.Join(Environment.NewLine, VariableExpressions.Select(x => $"{LEFT_PADDING}{x.Key} = " + x.Value.ToString().StripParentheses()));
             }
 
+            // Add variable values.
             if (VariableValues.Count > 0)
             {
                 if (!string.IsNullOrEmpty(str))
