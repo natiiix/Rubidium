5d9cadbec007fecb5feb749a3482db8c6c40cb0c
natiiix <natiwastaken@gmail.com>
2019-02-11 23:12:00 +0100

    Add ParameterSeparator property to SpecialToken

--- Rubidium/src/Token/SpecialToken.cs
+++ Rubidium/src/Token/SpecialToken.cs
@@ -7,6 +7,7 @@ namespace Rubidium
     public class SpecialToken : Token
     {
         public bool Terminator => StringValue == ";";
+        public bool ParameterSeparator => StringValue == ",";
         public bool Equality => StringValue == "=";
         public bool LeftParenthesis => StringValue == "(";
         public bool RightParenthesis => StringValue == ")";
