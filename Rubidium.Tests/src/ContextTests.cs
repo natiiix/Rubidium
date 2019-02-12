7a0a0c580eaeaa394227454bce131817d4bddf7b
natiiix <natiwastaken@gmail.com>
2019-02-12 09:01:51 +0100

    Move custom function tests from ContextTests to FunctionTests

--- Rubidium.Tests/src/ContextTests.cs
+++ Rubidium.Tests/src/ContextTests.cs
@@ -62,48 +62,5 @@ namespace Rubidium.Tests
             Assert.Equal(6, c.VariableValues["x1"]);
             Assert.Equal(-2, c.VariableValues["x2"]);
         }
-
-        [Fact]
-        public static void TestFunctionCallSingleArg()
-        {
-            Context c = Program.Evaluate("x = abs(-42)");
-
-            Assert.Empty(c.Statements);
-            Assert.Empty(c.VariableExpressions);
-
-            Assert.Single(c.VariableValues);
-            Assert.True(c.VariableValues.ContainsKey("x"));
-            Assert.Equal(42, c.VariableValues["x"]);
-        }
-
-        [Fact]
-        public static void TestFunctionCallMultiArg()
-        {
-            Context c = Program.Evaluate("x = max(10, -20, 42, -785)");
-
-            Assert.Empty(c.Statements);
-            Assert.Empty(c.VariableExpressions);
-
-            Assert.Single(c.VariableValues);
-            Assert.True(c.VariableValues.ContainsKey("x"));
-            Assert.Equal(42, c.VariableValues["x"]);
-        }
-
-        [Fact]
-        public static void TestFunctionCallSqrt()
-        {
-            Context c = Program.Evaluate("x = sqrt(65536); y = sqrt(0.01)");
-
-            Assert.Empty(c.Statements);
-            Assert.Empty(c.VariableExpressions);
-
-            Assert.Equal(2, c.VariableValues.Count);
-
-            Assert.True(c.VariableValues.ContainsKey("x"));
-            Assert.Equal(256, c.VariableValues["x"]);
-
-            Assert.True(c.VariableValues.ContainsKey("y"));
-            Assert.Equal((Fraction)0.1, c.VariableValues["y"]);
-        }
     }
 }
---
+++ Rubidium.Tests/src/FunctionTests.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+using Rubidium;
+
+namespace Rubidium.Tests
+{
+    public static class FunctionTests
+    {
+        [Fact]
+        public static void TestFunctionCallSingleArg()
+        {
+            Context c = Program.Evaluate("x = abs(-42)");
+
+            Assert.Empty(c.Statements);
+            Assert.Empty(c.VariableExpressions);
+
+            Assert.Single(c.VariableValues);
+            Assert.True(c.VariableValues.ContainsKey("x"));
+            Assert.Equal(42, c.VariableValues["x"]);
+        }
+
+        [Fact]
+        public static void TestFunctionCallMultiArg()
+        {
+            Context c = Program.Evaluate("x = max(10, -20, 42, -785)");
+
+            Assert.Empty(c.Statements);
+            Assert.Empty(c.VariableExpressions);
+
+            Assert.Single(c.VariableValues);
+            Assert.True(c.VariableValues.ContainsKey("x"));
+            Assert.Equal(42, c.VariableValues["x"]);
+        }
+
+        [Fact]
+        public static void TestFunctionCallSqrt()
+        {
+            Context c = Program.Evaluate("x = sqrt(65536); y = sqrt(0.01)");
+
+            Assert.Empty(c.Statements);
+            Assert.Empty(c.VariableExpressions);
+
+            Assert.Equal(2, c.VariableValues.Count);
+
+            Assert.True(c.VariableValues.ContainsKey("x"));
+            Assert.Equal(256, c.VariableValues["x"]);
+
+            Assert.True(c.VariableValues.ContainsKey("y"));
+            Assert.Equal((Fraction)0.1, c.VariableValues["y"]);
+        }
+    }
+}
